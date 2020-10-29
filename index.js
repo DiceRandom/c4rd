8d75457087022471538a1be06b18d91dd734ead9
Dice A <51027869+DiceRandom@users.noreply.github.com>
2020-10-29 15:00:51 -0400

    UI Update

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("Loaded");
+
+var url = window.location.href;
+var data = JSON.parse(url.split("#")[1].replace(/%22/g,'"').replace(/%20/g,' '));
+console.log(data);
+
+var name = data.name;
+document.getElementById("name").innerHTML = name;
+
+
+function createExample(){
+	window.location.href = '#{"name":"John","age":30,"city":"New York"}';
+	var url = window.location.href;
+	var data = JSON.parse(url.split("#")[1].replace(/%22/g,'"').replace(/%20/g,' '));
+}
